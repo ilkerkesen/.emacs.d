57937dafe10d443e134266dbc4ea3a1936bb20c0
Ilker Kesen <ilkerksn@gmail.com>
2017-02-07 12:10:22 +0300

    use undo-tree

--- init.el
+++ init.el
@@ -81,9 +81,9 @@
 (global-set-key (kbd "C-c C-y v") 'yas-visit-snippet)
 
 ;; Julia
-;; (load "~/.emacs.d/ESS/lisp/ess-site")
-;; (defvar inferior-julia-program-name "/usr/bin/julia")
-;; (add-hook 'julia-mode-hook #'smartparens-mode)
+(load "~/.emacs.d/ESS/lisp/ess-site")
+(defvar inferior-julia-program-name "/usr/bin/julia")
+(add-hook 'julia-mode-hook #'smartparens-mode)
 
 ;; orgmode
 (require 'org)
@@ -125,3 +125,6 @@
             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
 (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
 (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
+
+;; undo-tree
+(global-undo-tree-mode)
